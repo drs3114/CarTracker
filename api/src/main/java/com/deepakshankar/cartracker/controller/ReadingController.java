57b44251ad52f09f44e1dc7e42ad7e0ca5113216
Deepak Shankar <drs3114@rit.edu>
2017-12-26 08:19:46 -0500

    feat(module-api): add service deligation

--- api/src/main/java/com/deepakshankar/cartracker/controller/ReadingController.java
+++ api/src/main/java/com/deepakshankar/cartracker/controller/ReadingController.java
@@ -1,6 +1,11 @@
 package com.deepakshankar.cartracker.controller;
 
 import com.deepakshankar.cartracker.entity.Reading;
+import com.deepakshankar.cartracker.entity.Vehicle;
+import com.deepakshankar.cartracker.exceptions.VehicleNotFoundException;
+import com.deepakshankar.cartracker.service.ReadingService;
+import com.deepakshankar.cartracker.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "readings")
 public class ReadingController {
 
+
+    @Autowired
+    private ReadingService readingService;
+
+    @Autowired
+    private VehicleService vehicleService;
+
     @RequestMapping(method = RequestMethod.POST)
     public void addReading(@RequestBody final Reading reading){
+
+        Vehicle vehicle = vehicleService.findOne(reading.getVin());
+        if(vehicle!=null){
+            readingService.create(reading);
+        }
+        else{
+            throw new VehicleNotFoundException();
+        }
 
     }
 }
--- api/src/main/java/com/deepakshankar/cartracker/controller/VehicleController.java
+++ api/src/main/java/com/deepakshankar/cartracker/controller/VehicleController.java
@@ -1,6 +1,8 @@
 package com.deepakshankar.cartracker.controller;
 
 import com.deepakshankar.cartracker.entity.Vehicle;
+import com.deepakshankar.cartracker.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,20 +11,45 @@ import java.util.List;
 @RequestMapping(path = "vehicles")
 public class VehicleController {
 
+    @Autowired
+    private VehicleService vehicleService;
+
     @RequestMapping(method = RequestMethod.GET)
     public List<Vehicle> findAll(){
 
-        return null;
+        return vehicleService.findAll();
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "{id}")
-    public Vehicle findOne(@PathVariable("id") String vin){
-        return null;
+    public Vehicle findOne(@PathVariable("id") final String vin){
+
+        return vehicleService.findOne(vin);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
-    public List<Vehicle> update(@RequestBody List<Vehicle> vehicles){
-        return null;
+    public List<Vehicle> update(@RequestBody final List<Vehicle> vehicles){
+
+        return vehicleService.update(vehicles);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "{id}")
+    public Vehicle update(@PathVariable("id") final String id,@RequestBody final Vehicle vehicle){
+
+        return vehicleService.update(vehicle);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Vehicle create(@RequestBody final Vehicle vehicle){
+
+        return vehicleService.create(vehicle);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "{id}")
+    public void delete(@PathVariable("id") final String vin,@RequestBody final Vehicle vehicle){
+
+        vehicleService.delete(vin,vehicle);
+
     }
 
 
