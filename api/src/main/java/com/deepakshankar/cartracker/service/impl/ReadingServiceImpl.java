53ccd120f0041c8fd43443934b53287a38b0ee7d
Deepak Shankar <drs3114@rit.edu>
2017-12-26 08:13:10 -0500

    feat(module-api): add service implementations

--- api/src/main/java/com/deepakshankar/cartracker/service/impl/ReadingServiceImpl.java
+++ api/src/main/java/com/deepakshankar/cartracker/service/impl/ReadingServiceImpl.java
@@ -1,13 +1,25 @@
 package com.deepakshankar.cartracker.service.impl;
 
+import com.deepakshankar.cartracker.dao.ReadingDao;
 import com.deepakshankar.cartracker.entity.Reading;
+import com.deepakshankar.cartracker.exceptions.BadRequestException;
 import com.deepakshankar.cartracker.service.ReadingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReadingServiceImpl implements ReadingService {
-    @Override
-    public void add(final Reading reading) {
 
+    @Autowired
+    private ReadingDao dao;
+
+    @Override
+    public void create(final Reading reading) {
+        if(reading!=null){
+            dao.save(reading);
+        }
+        else{
+            throw new BadRequestException("Reading cannot be empty!");
+        }
     }
 }
--- api/src/main/java/com/deepakshankar/cartracker/service/impl/VehicleServiceImpl.java
+++ api/src/main/java/com/deepakshankar/cartracker/service/impl/VehicleServiceImpl.java
@@ -1,40 +1,70 @@
 package com.deepakshankar.cartracker.service.impl;
 
+import com.deepakshankar.cartracker.dao.VehicleDao;
 import com.deepakshankar.cartracker.entity.Vehicle;
+import com.deepakshankar.cartracker.exceptions.BadRequestException;
+import com.deepakshankar.cartracker.exceptions.VehicleNotFoundException;
 import com.deepakshankar.cartracker.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
+    @Autowired
+    private VehicleDao vehicleDao;
+
     @Override
     public Vehicle findOne(final String id) {
-        return null;
+
+        return vehicleDao.findOne(id);
+
     }
 
     @Override
     public List<Vehicle> findAll() {
-        return null;
+
+        return vehicleDao.findAll();
     }
 
     @Override
     public List<Vehicle> update(final List<Vehicle> vehicles) {
-        return null;
+
+        vehicles.forEach(vehicle -> {
+            update(vehicle);
+        });
+
+        return vehicles;
     }
 
     @Override
     public Vehicle update(final Vehicle vehicle) {
-        return null;
+        if (vehicle != null && vehicle.getVin() != null && vehicle.getVin() != "" && vehicle.getVin().length() > 0) {
+            vehicleDao.save(vehicle);
+        }
+        else{
+            if(vehicle==null){
+                throw new VehicleNotFoundException();
+            }
+            throw new BadRequestException("Invalid VIN number!");
+        }
+        return vehicleDao.findOne(vehicle.getVin());
     }
 
     @Override
     public Vehicle create(final Vehicle vehicle) {
-        return null;
+
+        return update(vehicle);
+
     }
 
     @Override
     public void delete(final String id, final Vehicle vehicle) {
-
+        Vehicle v = vehicleDao.findOne(id);
+        if (v == null) {
+            throw new VehicleNotFoundException();
+        }
+        vehicleDao.delete(vehicle);
     }
 }
