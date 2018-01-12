e716e96399b36bf9e941d3c625f2973c34cf33a0
Deepak Shankar <drs3114@rit.edu>
2018-01-11 23:13:43 -0500

    feat(module-client): add alerts service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MatButtonModule, MatCardModule, MatChipsModule, MatDialogModule, MatGridListModule, MatToolbarModule} from '@angular/material';
+import {CarDialogComponent, DashboardComponent} from './dashboard/dashboard.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {CarService} from './car.service';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './/app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AlertsComponent } from './alerts/alerts.component';
+import { AlertsService } from './alerts.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CarDialogComponent,
+    AlertsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    AppRoutingModule,
+    MatChipsModule
   ],
-  providers: [],
+  providers: [CarService, AlertsService],
+  entryComponents: [
+    DashboardComponent,
+    CarDialogComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
