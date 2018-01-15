884e6a074a74bf89aface066a2a248d9cb4e6124
Deepak Shankar <drs3114@rit.edu>
2018-01-15 01:15:58 -0500

    refactor(module-client): remove unwanted config for charts

--- client/src/app/components/chart/chart.component.ts
+++ client/src/app/components/chart/chart.component.ts
@@ -14,25 +14,9 @@ export class ChartComponent implements OnInit {
   };
 
   public lineChartColors: Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
+    {
+      backgroundColor: 'rgba(143, 19, 129, 0.5)',
+      borderColor: 'rgba(140, 114, 137,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
